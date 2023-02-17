a511c9d6fb978bd296b93dada3b07c6d46f6dbf8
Vinicius Barbosa <vinicius-s8u>
2023-02-17 16:25:42 -0300

    1.0

---
+++ api-schedule/src/test/java/br/com/ezschedule/apischedule/ApiScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ezschedule.apischedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
