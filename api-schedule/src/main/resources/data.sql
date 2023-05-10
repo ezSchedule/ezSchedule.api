a63302275016e5183b66b031b2453c9b4b669bb9
Endryl Fiorotti <endryl.fiorotti@sptech.school>
2023-05-10 11:10:00 -0300

    :sparkles: Endpoint da tela de pagamentos terminada!

--- api-schedule/src/main/resources/data.sql
+++ api-schedule/src/main/resources/data.sql
@@ -1,2 +1,5 @@
 INSERT INTO Condominium(name,country,city,state,street,postal_code) values
 ('Jojolion Condominium','roads take me home','of Dreams','of mother Russia','Vila Sesamo st','0666-924');
+
+INSERT INTO Saloon (saloon_name,saloon_price,saloon_block, condominium_id) VALUES
+('SALON 1', 60.00, '1A', 1);
