7969e0dbb0ade38408ff7cb91d991f665223ce15
Vinicius Barbosa <vinicius.barbosa@sptech.school>
2023-11-03 06:43:57 -0300

    :sparkles: test for webHook

--- api-schedule/src/main/java/br/com/ezschedule/apischedule/controller/PixController.java
+++ api-schedule/src/main/java/br/com/ezschedule/apischedule/controller/PixController.java
@@ -5,7 +5,10 @@ import br.com.ezschedule.apischedule.model.DtoClasses.efi.PixResponse;
 import br.com.ezschedule.apischedule.service.PixService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
@@ -18,8 +21,13 @@ public class PixController {
 
 
     @PostMapping
-    public ResponseEntity<PixResponse> createPix(@RequestBody @Valid PixRequest pix){
+    public ResponseEntity<PixResponse> createPix(@RequestBody @Valid PixRequest pix) {
         return pixService.createPix(pix);
+    }
+
+    @PostMapping("/webhook")
+    public ResponseEntity<Void> pixPayed() {
+        return ResponseEntity.status(200).build();
     }
 
 }
